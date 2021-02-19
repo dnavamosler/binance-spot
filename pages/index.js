04cb68baec853d0f8c66c14746ffc73fbb493746
daniel <dnavamoslercl@gmail.com>
2021-02-19 12:46:49 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import styles2 from "../styles/Home.module.scss";
+import hello from "./api/hello";
 
 export default function Home(datos) {
   // { invertido = 0, ganancia = 0, monedas = [] }
@@ -38,38 +39,49 @@ export default function Home(datos) {
               <td className={styles2.header}>INVERTIDO</td>
               <td className={styles2.header}>CANTIDAD</td>
               <td className={styles2.header}>PRECIO</td>
+              <td className={styles2.header}>DOBLAR INVERSION</td>
               <td className={styles2.header}>GANANCIA * 0.1</td>
 
               <td className={styles2.header}>BALANCE</td>
             </tr>
-            {monedas.map((moneda, i) => (
-              <tr key={i}>
-                <td className={styles2.mainCell}>{moneda.token}</td>
-                <td className={styles2.cell}>{moneda.invertido}</td>
-                <td className={styles2.cell}>{moneda.qt}</td>
-                <td className={styles2.cell}>{moneda.precioVenta}</td>{" "}
-                <td className={styles2.cell}>
-                  {Math.round(0.1 * moneda.qt * 100) / 100}
-                </td>
-                <td
-                  className={styles2.cell}
-                  style={{
-                    fontWeight: 600,
-                    fontSize: "1.1em",
-                    color: moneda.ganancia > 0 ? "#2e7d32" : "#c62828",
-                  }}
-                >
-                  {Math.round(moneda.ganancia * 100) / 100}
-                </td>
-              </tr>
-            ))}
+            {monedas.map((moneda, i) => {
+              return (
+                <tr key={i}>
+                  <td className={styles2.mainCell}>{moneda.token}</td>
+                  <td className={styles2.cell}>{moneda.invertido}</td>
+                  <td className={styles2.cell}>{moneda.qt}</td>
+                  <td className={styles2.cell}>{moneda.precioVenta}</td>{" "}
+                  <td className={styles2.cell}>
+                    {Math.round(
+                      ((moneda.precioVenta * (moneda.invertido * 2)) /
+                        moneda.invertido) *
+                        100
+                    ) / 100}{" "}
+                    $
+                  </td>{" "}
+                  <td className={styles2.cell}>
+                    {Math.round(0.1 * moneda.qt * 100) / 100}
+                  </td>
+                  <td
+                    className={styles2.cell}
+                    style={{
+                      fontWeight: 600,
+                      fontSize: "1.1em",
+                      color: moneda.ganancia > 0 ? "#2e7d32" : "#c62828",
+                    }}
+                  >
+                    {Math.round(moneda.ganancia * 100) / 100}
+                  </td>
+                </tr>
+              );
+            })}
             <tr>
               <td className={styles2.mainCell}>TOTAL</td>
               <td colSpan={2} style={{ textAlign: "center" }}>
                 {Math.round(invertido * 100) / 100} USD
               </td>
               <td
-                colSpan={3}
+                colSpan={4}
                 style={{
                   fontWeight: 600,
                   fontSize: "1.3em",
