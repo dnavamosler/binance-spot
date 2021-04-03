b582fd69222129936f711f96652d3185a90a19ab
dnavamosler <45084779+dnavamosler@users.noreply.github.com>
2021-04-02 21:14:14 -0300

    aka

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home(datos) {
       } = await res.json();
 
       setValores({ monedas: data, invertido, ganancia });
-    }, 1200);
+    }, 5000);
   }, []);
 
   return (
