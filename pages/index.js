1ee8ae6c6e620c4d48a5080d03d79e666a6b13b2
dnavamosler <45084779+dnavamosler@users.noreply.github.com>
2021-04-14 01:17:22 -0400

    nice

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import useSwr from "swr";
-
+import Image from "next/image";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home(datos) {
@@ -18,65 +18,86 @@ export default function Home(datos) {
     }
   }, [data]);
 
-  if (!data && !error) {
-    return (
-      <div>
-        <span>cargando...</span>
-      </div>
-    );
-  }
+  const cargando = !data && !error;
+  // const cargando = true;
+
   return (
     <div className="flex justify-center flex-wrap bg-blue-200 absolute top-0 left-0 right-0 bottom-0">
-      <div className="w-full max-w-lg  mt-16  ">
-        <div className="grid grid-cols-5 bg-white rounded-lg shadow-lg py-2">
-          {headers.map((token) => (
-            <div
-              key={token}
-              className="p-2 border-b-2 text-right border-blue-700 text-sm text-blue-600 font-semibold"
-            >
-              {token}
+      {cargando ? (
+        <Loading />
+      ) : (
+        <div className="w-full max-w-lg  mt-16  ">
+          <div className="mb-4 flex justify-center">
+            <div className="flex flex-col">
+              <div className="flex flex-col">
+                <span className="font-light text-center text-blue-600 text-lg">
+                  Wallet
+                </span>{" "}
+                <span className="font-semibold text-center text-blue-800 text-2xl">
+                  Daniel Nava
+                </span>
+              </div>
             </div>
-          ))}
-          {data.map((asset) => {
-            return (
-              <>
-                <div className="px-2 font-light border-b border-blue-200 text-right">
-                  {asset.asset}
-                </div>
-                <div className="px-2 border-b border-blue-200 text-right">
-                  {Math.round(asset.invertido * 10) / 10}
-                </div>
-                <div className="px-2 border-b border-blue-200 text-right">
-                  {Math.round(asset.qt * 100) / 100}
-                </div>
-                <div className="px-2 border-b border-blue-200 text-right">
-                  {Math.round(asset.precio * 1000) / 1000}
-                </div>
-                <div
-                  className={`px-2 border-b border-blue-200 text-right font-black ${
-                    asset.balance > 0 ? "text-green-600" : "text-red-600"
-                  }`}
-                >
-                  {Math.round(asset.balance * 10) / 10}
-                </div>
-              </>
-            );
-          })}
-        </div>
+          </div>
+          <div className="grid grid-cols-5 bg-white rounded-lg shadow-lg py-2">
+            {headers.map((token) => (
+              <div
+                key={token}
+                className="p-2 border-b-2 text-right border-blue-700 text-sm text-blue-600 font-semibold"
+              >
+                {token}
+              </div>
+            ))}
+            {data.map((asset) => {
+              return (
+                <>
+                  <div className="px-2 font-light border-b border-blue-200 text-right">
+                    {asset.asset}
+                  </div>
+                  <div className="px-2 border-b border-blue-200 text-right">
+                    {Math.round(asset.invertido * 10) / 10}
+                  </div>
+                  <div className="px-2 border-b border-blue-200 text-right">
+                    {Math.round(asset.qt * 100) / 100}
+                  </div>
+                  <div className="px-2 border-b border-blue-200 text-right">
+                    {Math.round(asset.precio * 1000) / 1000}
+                  </div>
+                  <div
+                    className={`px-2 border-b border-blue-200 text-right font-black ${
+                      asset.balance > 0 ? "text-green-600" : "text-red-600"
+                    }`}
+                  >
+                    {Math.round(asset.balance * 10) / 10}
+                  </div>
+                </>
+              );
+            })}
+          </div>
 
-        <div className=" mt-10">
-          <h1 className="text-center text-blue-700 font-semibold">
-            BALANCE ACTUAL
-          </h1>
-          <p
-            className={`text-center font-bold text-4xl ${
-              BALANCE > 0 ? "text-green-600" : "text-red-600"
-            }`}
-          >
-            {BALANCE} USD
-          </p>
+          <div className=" mt-10">
+            <h1 className="text-center text-blue-700 font-semibold">
+              BALANCE ACTUAL
+            </h1>
+            <p
+              className={`text-center font-bold text-4xl ${
+                BALANCE > 0 ? "text-green-600" : "text-red-600"
+              }`}
+            >
+              {BALANCE} USD
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <Image src="/cargando.svg" width={300} height={300} />
+      <h3 className="text-blue-600 text-4xl">Cargando...</h3>
+    </div>
+  );
+};
