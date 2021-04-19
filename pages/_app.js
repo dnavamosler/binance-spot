0f8202847a0aa95cff2607347dc5f5ffb46ccff8
daniel <dnavamoslercl@gmail.com>
2021-04-19 10:36:29 -0400

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,11 @@ function MyApp({ Component, pageProps }) {
         <title>Crypto wallet</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#90cdf4" />
+        <meta name="theme-color" content="#90cdf4" />{" "}
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </Head>
       <Component {...pageProps} />;
     </>
